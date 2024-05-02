8cd4adb42d37fce48ee0594053834290cd3977a6
fedor <parashchenko.fedor@gmail.com>
2024-05-02 22:32:06 +0300

    fix: ContactNote Schema change required option of 2 fields to 'false'

--- api/src/models/ContactNote.ts
+++ api/src/models/ContactNote.ts
@@ -2,10 +2,10 @@ import {Schema, model} from 'mongoose'
 
 const ContactNote = new Schema({
     theme: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, required: false },
     name: { type: String, required: true },
     lastname: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String, required: false },
     email: { type: String, required: true }
 })
 
